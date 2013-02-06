fccdf8fe07fccaef21ddae1dbe6d53d6ad80447a
LinuxTzs <linuxtzs@gmail.com>
2013-02-06 20:46:29 +0800

    建立Code::Blocks工程

---
+++ include/config.h
@@ -0,0 +1,16 @@
+// stock library reference switches
+#define USE_GFX 0
+#define USE_EEPROM 0
+#define USE_ETHERNET 0
+#define USE_FIRMATA 0
+#define USE_LCD 0
+#define USE_LCD4884 0
+#define USE_OBD 0
+#define USE_SD 0
+#define USE_SERVO 0
+#define USE_SOFTSERIAL 0
+#define USE_SPI 0
+#define USE_STEPPER 0
+#define USE_TINYGPS 0
+#define USE_WIFI 0
+#define USE_WIRE 0
