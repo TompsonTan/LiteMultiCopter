142e2bc940ecb963081a8f6b428b679053769a5d
kehengsite <lq52655963@163.com>
2013-03-27 15:05:18 +0800

    2013.03.27
    添加单个电机测试

---
+++ SingleMotorTest.cpp
@@ -0,0 +1,27 @@
+#define analogPin 9        #将9号管脚定义为输出管脚
+
+int val;                   #添加变量
+
+void setup()
+ {
+   Serial.begin(9600);     #起始波特率
+ }
+
+void loop()
+{
+  
+   for(val=43;val<=255;val=val+1)
+   {
+     analogWrite(analogPin,val)； 
+   }
+   
+   Serial.println(val);           
+   #输出最大值
+   for(val=255;val>=43;val=val-1)
+   {
+     analogWrite(analogPin,val);
+   }  
+   
+  Serial.println(val);          
+  #输出最小值
+}
